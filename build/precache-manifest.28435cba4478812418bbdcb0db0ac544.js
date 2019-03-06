1237ca6fff515334174eb032b4d9a0524745fafe
beatrizf13 <azz.felix7@gmail.com>
2019-03-06 01:14:08 -0300

    deploy

---
+++ build/precache-manifest.28435cba4478812418bbdcb0db0ac544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3bc148be67c8329b989",
+    "url": "/static/js/main.c3bc148b.chunk.js"
+  },
+  {
+    "revision": "db025ab40ce83d5f9d02",
+    "url": "/static/js/1.db025ab4.chunk.js"
+  },
+  {
+    "revision": "db025ab40ce83d5f9d02",
+    "url": "/static/css/1.0b7d3b1f.chunk.css"
+  },
+  {
+    "revision": "ce80e91e42f377f2ce4ef63975ff63dc",
+    "url": "/index.html"
+  }
+];
